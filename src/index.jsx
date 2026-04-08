3caf46ad04ea772db4d8502d7b4dac6eae878e40
rowando <khanhdg3007@gmail.com>
2026-04-09 01:18:15 +0700

    feat: initialize Meko Banana Pro Photoshop plugin with manifest, entrypoints, and panel controller

--- src/index.jsx
+++ src/index.jsx
@@ -2,15 +2,19 @@ import React from "react";
 
 import "./styles.css";
 import { PanelController } from "./controllers/PanelController.jsx";
-import { CommandController } from "./controllers/CommandController.jsx";
-import { About } from "./components/About.jsx";
 import { App } from "./panels/App.jsx";
 
-import { entrypoints } from "uxp";
+import { entrypoints, shell } from "uxp";
+
+const ABOUT_WEBSITE_URL = "https://www.mekomedia.vn/p/meko-banana-pro.html";
 
 const appController = new PanelController(() => <App/>, { id: "demos", menuItems: [
     { id: "reload1", label: "Reload Plugin", enabled: true, checked: false, oninvoke: () => location.reload() }
 ] });
+
+const openAboutWebsite = async () => {
+    await shell.openExternal(ABOUT_WEBSITE_URL);
+};
 
 entrypoints.setup({
     plugin: {
@@ -20,6 +24,9 @@ entrypoints.setup({
         destroy() {
             console.log("destroyed");
         }
+    },
+    commands: {
+        showAbout: openAboutWebsite
     },
     panels: {
         demos: appController
